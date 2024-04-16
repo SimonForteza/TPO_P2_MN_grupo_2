f1e3b4155bfa291d62760c2b651151db6f49b310
Simon Forteza <fortezasimon@gmail.com>
2024-04-16 13:42:56 -0300

    Resolución punto 1 (calcular transpuesta) - ejercicio 1

--- src/main/java/org/example/ejercicio_1/Exercise_1.java
+++ src/main/java/org/example/ejercicio_1/Exercise_1.java
@@ -1,4 +1,25 @@
 package org.example.ejercicio_1;
 
+import org.example.ejercicio_1.tda.IQueueOfStacks;
+import org.example.ejercicio_1.tda.IStack;
+
 public class Exercise_1 {
+    public static int getTrace(IQueueOfStacks queue) {
+        int trace = 0;
+        int count = 1;
+
+        while (!queue.isEmpty()){
+            IStack stack = queue.getFirst();
+
+            int candidate = 0;
+            for (int i = 0; i < count; i++) {
+                candidate = stack.getTop();
+                stack.remove();
+            }
+            trace += candidate;
+            queue.remove();
+            count++;
+        }
+        return trace;
+    }
 }
