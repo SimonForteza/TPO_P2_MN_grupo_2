18bf3768dce55b702dbfd67413da8da90c69de4c
Simon Forteza <fortezasimon@gmail.com>
2024-04-17 10:39:41 -0300

    creacion de metodo invertQueueofStack y pruebas en Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,36 @@
 package org.example;
 
+import org.example.ejercicio_1.Exercise_1;
+import org.example.ejercicio_1.tda.IQueueOfStacks;
+import org.example.ejercicio_1.tda.IStack;
+import org.example.ejercicio_1.tda.QueueOfStacks;
+import org.example.ejercicio_1.tda.Stack;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //main file
+        IStack stack1 = new Stack();
+        stack1.add(9);
+        stack1.add(8);
+        stack1.add(7);
+
+        IStack stack2 = new Stack();
+        stack2.add(6);
+        stack2.add(5);
+        stack2.add(4);
+        IStack stack3 = new Stack();
+        stack3.add(3);
+        stack3.add(2);
+        stack3.add(100);
+
+        IQueueOfStacks queue = new QueueOfStacks();
+        queue.add(stack1);
+        queue.add(stack2);
+        queue.add(stack3);
+
+        System.out.println(Exercise_1.getTrace(queue));
+        //IQueueOfStacks transpose = Exercise_1.getTranpose(queue);
     }
 }
--- src/main/java/org/example/ejercicio_1/Exercise_1.java
+++ src/main/java/org/example/ejercicio_1/Exercise_1.java
@@ -1,10 +1,10 @@
 package org.example.ejercicio_1;
 
-import org.example.ejercicio_1.tda.IQueueOfStacks;
-import org.example.ejercicio_1.tda.IStack;
+import org.example.ejercicio_1.tda.*;
 
 public class Exercise_1 {
     public static int getTrace(IQueueOfStacks queue) {
+        queue = invertQueueOfStacks(queue);
         int trace = 0;
         int count = 1;
 
@@ -22,4 +22,55 @@ public class Exercise_1 {
         }
         return trace;
     }
+
+    private static IQueueOfStacks invertQueueOfStacks(IQueueOfStacks queue) {
+        IQueueOfStacks invertedQueue = new QueueOfStacks();
+        IStackOfStacks stack = new StackOfStacks();
+        while (!queue.isEmpty()) {
+            IStack first = queue.getFirst();
+            stack.add(first);
+            queue.remove();
+        }
+
+        while (!stack.isEmpty()) {
+            IStack top = stack.getTop();
+            invertedQueue.add(top);
+            stack.remove();
+        }
+        return invertedQueue;
+    }
+
+    /*
+    public static IQueueOfStacks getTranpose(IQueueOfStacks queue) {
+        IQueueOfStacks transpose = new QueueOfStacks();
+
+        while (!queue.isEmpty()) {
+            IQueueOfStacks aux = new QueueOfStacks();
+            IStack stack_transpose = new Stack();
+
+            IStack firstStack = queue.getFirst();
+            boolean isTopElement = true;
+
+            while (!firstStack.isEmpty() && isTopElement == true) {
+                int top = firstStack.getTop();
+                firstStack.remove();
+                stack_transpose.add(top); // agrego elemento a la pila de transpues
+                isTopElement = false;
+            }
+
+            transpose.add(stack_transpose);
+
+            aux.add(firstStack);
+            queue.remove();
+
+            while (!aux.isEmpty()) {
+                IStack first = aux.getFirst();
+                queue.add(first);
+                aux.remove();
+            }
+        }
+
+        return transpose;
+    }
+     */
 }
