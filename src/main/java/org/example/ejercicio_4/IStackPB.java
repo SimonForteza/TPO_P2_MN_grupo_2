df76c7ce20b8b5c1b32dd8e61d26a68bd2003242
Simon Forteza <fortezasimon@gmail.com>
2024-06-25 20:16:40 -0300

    Realizacion ejercicio 4

---
+++ src/main/java/org/example/ejercicio_4/IStackPB.java
@@ -0,0 +1,11 @@
+package org.example.ejercicio_4;
+
+public interface IStackPB {
+    IStackPB add(int x);
+
+    void remove();
+
+    boolean isEmpty();
+
+    int getTop();
+}
---
+++ src/main/java/org/example/ejercicio_4/Queue.java
@@ -0,0 +1,53 @@
+package org.example.ejercicio_4;
+
+public class Queue implements IQueue {
+
+    private final int[] array;
+    private int count;
+
+    public Queue() {
+        this.array = new int[10000];
+        this.count = 0;
+    }
+    public Queue(int... elements) { //constructor con parametros variables
+        this.array = new int[10000];
+        this.count = 0;
+        for (int element : elements) {
+            add(element);
+        }
+    }
+
+
+    @Override
+    public void add(int a) {
+        if (count == 10000) {
+            throw new RuntimeException("La cola está llena");
+        }
+        array[count] = a;
+        count++;
+    }
+
+    @Override
+    public void remove() {
+        if (isEmpty()) {
+            throw new RuntimeException("pila está vacia");
+        }
+        for(int i = 0; i < count - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        count--;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    @Override
+    public int getFirst() {
+        if (isEmpty()) {
+            throw new RuntimeException("La pila está vacía");
+        }
+        return array[0];
+    }
+}
